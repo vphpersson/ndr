de03632160e111c6742011c24ddf9ca3dfab30bc
Viktor Persson <vph.persson@gmail.com>
2021-08-02 21:42:44 +0200

    Rework pointer code and remove `Pipfile`.
      - The `data` input type is now `ByteString`.
      - The `representation` type now uses `memoryview` instead of `bytes`.
      - Use `venv` instead of `Pipfile`.

--- ndr/structures/pointer.py
+++ ndr/structures/pointer.py
@@ -1,35 +1,38 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
-from typing import Union, ClassVar
-from struct import unpack as struct_unpack, pack as struct_pack
+from typing import Union, ClassVar, ByteString
+from struct import unpack_from, pack
 
 from ndr.structures import NDRType
 
 
 @dataclass
 class Pointer(NDRType):
-    representation: Union[NDRType, bytes]
+    representation: Union[NDRType, memoryview]
     referent_id: int = field(default_factory=lambda: next(NDRType._referent_id_iterator))
     structure_size: ClassVar[int] = 4
 
     @classmethod
-    def from_bytes(cls, data: bytes) -> Pointer:
+    def from_bytes(cls, data: ByteString, base_offset: int = 0) -> Pointer:
+        data = memoryview(data)[base_offset:]
+        offset = 0
 
-        referent_id: int = struct_unpack('<I', data[:4])[0]
+        referent_id: int = unpack_from('<I', buffer=data, offset=offset)[0]
+        offset += 4
         if referent_id == 0:
             return NullPointer()
 
         return cls(
-            referent_id=struct_unpack('<I', data[:4])[0],
-            representation=data[4:]
+            referent_id=referent_id,
+            representation=data[offset:]
         )
 
     def __bytes__(self) -> bytes:
-        return struct_pack('<I', self.referent_id) + bytes(self.representation)
+        return pack('<I', self.referent_id) + bytes(self.representation)
 
 
 # TODO: I want this to be a singleton...
 class NullPointer(Pointer):
     def __init__(self):
-        self.representation = b''
+        self.representation = memoryview(b'')
         self.referent_id = 0
