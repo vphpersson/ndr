fbe3bbc947f19141a277c94c121bef4204f825a9
Viktor Persson <vph.persson@gmail.com>
2021-08-06 21:24:25 +0200

    Update `UnidimensionalConformantArray.from_bytes`.
      - Change `data` type to `ByteString`.
      - Add `base_offset` parameter.
      - Remove `try`-`catch`.
      - Use `Struct`.

--- ndr/structures/unidimensional_conformant_array.py
+++ ndr/structures/unidimensional_conformant_array.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import ClassVar, Tuple, Union
-from struct import unpack as struct_unpack, pack as struct_pack, error as struct_error, \
-    unpack_from as struct_unpack_from
+from typing import ClassVar, Tuple, Union, ByteString
+from struct import Struct, unpack_from
 
 from ndr.structures import NDRType
 
@@ -10,24 +9,27 @@ from ndr.structures import NDRType
 @dataclass
 class UnidimensionalConformantArray(NDRType):
     STRUCTURE_SIZE: ClassVar[int] = 4
+
+    _MAXIMUM_COUNT_STRUCT: ClassVar[Struct] = Struct('<I')
+
     representation: Tuple[Union[NDRType, bytes], ...]
 
     @classmethod
-    def from_bytes(cls, data: bytes, size_per_element: int = 1) -> UnidimensionalConformantArray:
-        try:
-            maximum_count: int = struct_unpack('<I', data[:4])[0]
-            return cls(
-                representation=tuple(
-                    bytes(struct_unpack_from(size_per_element * 'B', buffer=data[4:], offset=i*size_per_element))
-                    for i in range(maximum_count)
-                )
+    def from_bytes(cls, data: ByteString, base_offset: int = 0, size_per_element: int = 1) -> UnidimensionalConformantArray:
+        data = memoryview(data)[base_offset:]
+        offset = 0
+
+        maximum_count: int = cls._MAXIMUM_COUNT_STRUCT.unpack_from(buffer=data, offset=offset)[0]
+        return cls(
+            representation=tuple(
+                bytes(unpack_from(size_per_element * 'B', buffer=data, offset=offset+i*size_per_element))
+                for i in range(maximum_count)
             )
-        except struct_error as e:
-            raise ValueError from e
+        )
 
     def __bytes__(self) -> bytes:
         return b''.join([
-            struct_pack('<I', len(self.representation)),
+            self._MAXIMUM_COUNT_STRUCT.pack(len(self.representation)),
             b''.join(
                 (bytes([element]) if isinstance(element, int) else bytes(element))
                 for element in self.representation
